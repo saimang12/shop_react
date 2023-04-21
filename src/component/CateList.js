8a73ff8e4efdf8499ebb49bdbd311ffc3b89fd66
saimang12@naver.com <saimang12@naver.com>
2023-04-21 16:54:07 +0900

    2023-04-21

--- src/component/CateList.js
+++ src/component/CateList.js
@@ -1,25 +1,40 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 const CateList = ({ shop, cateList }) => {
     const { cate } = useParams();
     const cateItm = shop.filter(it => it.category === cate)
     return (
-        <section className="cateList">
-
-            <ul>
-                {
-                    cateItm.map(it => {
-                        return (
-                            <li key={it.id}>
-                                {
-                                    it.name
-                                }
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+        <section className="CateList">
+            <div className="inner">
+                <ul>
+                    {
+                        cateItm.map(it => {
+                            return (
+                                <li key={it.id}>
+                                    <Link to={`/detali/${it.id}`}>
+                                        <figure>
+                                            <img src={it.api_featured_image} alt={it.name} />
+                                        </figure>
+                                    </Link>
+                                    <div className="desc">
+                                        <h3>{it.name}</h3>
+                                        <p>
+                                            {
+                                                it.description &&
+                                                it.description.substr(0, 60) + '...'
+                                                || 'This product has no description.'
+                                            }
+                                        </p>
+                                        <span>{it.price === '0.0' ? 'SoldOut' : it.price}
+                                            {it.price === '0.0' ? '' : it.price_sign}</span>
+                                    </div>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         </section>
     )
 }
