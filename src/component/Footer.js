b7d52f2c8aebbacc4fed6a488679b745592436b9
saimang12@naver.com <saimang12@naver.com>
2023-04-21 16:14:21 +0900

    2023-04-21

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="footer">
-            footer
+            Park Jinwoo shop project
         </footer>
     )
 }
